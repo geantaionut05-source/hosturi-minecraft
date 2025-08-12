b8c05201be7f2aad4f7b7845334b62c6e59d605f
ibrl_yt <geanta.ionut05@gmail.com>
2025-08-12 20:10:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ function genereazaIP() {
     const prefixe = ["mc", "sv", "play", "node", "srv"];
     const randomPrefix = prefixe[Math.floor(Math.random() * prefixe.length)];
     const randomNum = Math.floor(Math.random() * 900) + 100;
-    const domeniu = "minehant.gq";
+    const domeniu = "minehant.net";
     const porturi = [25565, 19132, 25575];
     const randomPort = porturi[Math.floor(Math.random() * porturi.length)];
     return `${randomPrefix}${randomNum}.${domeniu}:${randomPort}`;
 }
 
-app.get("/genereaza-ip", (req, res) => {
+app.get("/gen-ip", (req, res) => {
     const ip = genereazaIP();
     res.json({ ip });
 });
